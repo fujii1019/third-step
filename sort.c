cdbce8cfa84283d7e721e3f023f4bbcb3ef33a08
fujii1019 <f.kentarou1019@gmail.com>
2020-02-29 16:14:44 +0900

    add coment (C)

--- sort.c
+++ sort.c
@@ -16,13 +16,13 @@ int ary1[20]={65,80,67,35,58,60,72,75,68,92,36,50,2,58.5,46,42,78,62,84,70};
 int ary2[20]={44,87,100,63,52,60,58,73,55,86,29,56,89,23,65,84,64,27,86,84};
 int i;
 printf("------------science--------------------\n");
-
+//rikanosort
 qsort(( int * )ary1 , 20 , sizeof( ary1[0] ),int_sort);
 for( i = 0 ; i < 20 ; i++ ) {
     printf( "data[%d]=%d\n" , i , ary1[i] );
   }
 printf("------------engrish--------------------\n");
-
+//eigonosort
 qsort(( int * )ary2 , 20 , sizeof( ary2[0] ),int_sort);
 for( i = 0 ; i < 20 ; i++ ) {
     printf( "data[%d]=%d\n" , i , ary2[i] );
