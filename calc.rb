7f1dca3b3738d882d07fae011a0cd9743eb98aa2
fujii1019 <f.kentarou1019@gmail.com>
2020-02-29 16:08:13 +0900

    add coment (Ruby)

--- calc.rb
+++ calc.rb
@@ -2,10 +2,11 @@ require 'complex'
 ary1 =[65,80,67,35,58,60,72,75,68,92,36,50,2,58.5,46,42,78,62,84,70]
 sum=0
 hensa=0
-
+#goukei
 for i in ary1
 sum=sum+i
 end
+#heikin
 ave=sum/20
 for i in ary1
 hensa=hensa+(i-ave)*(i-ave)
@@ -21,11 +22,13 @@ end
 ary2 =[44,87,100,63,52,60,58,73,55,86,29,56,89,23,65,84,64,27,86,84]
 sum=0
 hensa=0
-
+#goukei
 for i in ary2
 sum=sum+i
 end
+#heikin
 ave=sum/20
+#hensa
 for i in ary2
 hensa=hensa+(i-ave)*(i-ave)
 end
